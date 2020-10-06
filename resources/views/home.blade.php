d44dffe436115c29c7c145499511c46f9f91c465
Marcelo Cunha <marcelogcunha7@gmail.com>
2020-10-06 13:39:10 +0100

    implementado listar todos os clientes na homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,6 +18,25 @@
     <div class="row">
         <div class="col">
             <h1>Lista de clientes</h1>
+            <table class="table table-striped">
+                <thead>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Empresa</th>
+                    <th scope="col"></th>
+                </thead>
+                <tbody>
+                    @foreach ($clientes as $cliente)
+                        <tr>
+                            <td>{{$cliente->nomeCliente}}</td>
+                            <td>{{$cliente->emailCliente}}</td>
+                            <td>{{$cliente->nomeEmpresaCliente}}</td>
+                            <td><a href="#" class="btn btn-primary btn-sm">Ver</a></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
         </div>
     </div>
 </div>
